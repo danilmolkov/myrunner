b35123a4e6da86d98f33803b85f4b9faf963a506
Danil Molkov <molkovdanil@gmail.com>
2024-05-29 21:29:56 +0200

    linter fix

--- myrunner/myrunner.py
+++ myrunner/myrunner.py
@@ -63,6 +63,7 @@ def loggingSetup():
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 if __name__ == '__main__':
     try:
         main()
